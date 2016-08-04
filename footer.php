f00443ba0b1d31680b6bef4343de2eca3ad1e0fe
Jack Machin <jack.machin125@gmail.com>
2016-08-04 11:18:46 +0100

    Update

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
 
                     <div class="subsidary-statement">
                         <a href="http://www.pfgl.co.uk"><img src="<?php echo get_template_directory_uri(); ?>/library/images/perspective-logo-small.png" alt="<?php bloginfo('name'); ?>" class="tiny-logo"></a>
-                        <p><?php bloginfo('name'); ?> is a subsidiary of Perspective Financial Group Limited.</p>
+                        <p><?php bloginfo('name'); ?> is proud to be part of Perspective Financial Group Limited.</p>
                     </div>
 
 				</div>
